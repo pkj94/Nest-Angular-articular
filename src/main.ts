8e32a32999c08eea7b0d7a0ef6d7b4c525c0620a
PKJ94 <pradeep.94@live.com>
2019-07-18 21:25:17 +0530

    test

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { join } from 'path';
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('/api');
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'client/dist/client'),
+    prefix: '/',
+  });
+  await app.listen(3000);
+}
+bootstrap();
